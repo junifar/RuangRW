b9f6dd1f170ca29bd1b110628b7d48a9d9963427
junifarhidayat <junifar@gmail.com>
2020-10-11 18:10:42 +0700

    search box component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Help from "./pages/help/Help";
-import Homev1 from "./pages/v1/home";
+import Home from "./pages/v2/home";
+// import { BrowserRouter, Route } from "react-router-dom";
+// import Home from "./pages/home/Home";
+// import About from "./pages/about/About";
+// import Help from "./pages/help/Help";
+// import Homev1 from "./pages/v1/home";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Route path="/" exact component={Home} />
         <Route path="/home" exact component={Home} />
         <Route path="/about" exact component={About} />
         <Route path="/help" exact component={Help} />
         <Route path="/v1/" exact component={Homev1} />
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Home />
     </div>
   );
 };
