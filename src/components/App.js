59a0bd8c5d3954fa7a6de4186cb605391ecc43a7
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-04-22 20:50:49 +0700

    Add latest react and material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,7 @@
-import React from "react";
-import Home from "./pages/v2/home";
-// import { BrowserRouter, Route } from "react-router-dom";
-// import Home from "./pages/home/Home";
-// import About from "./pages/about/About";
-// import Help from "./pages/help/Help";
-// import Homev1 from "./pages/v1/home";
+import Button from '@material-ui/core/Button';
 
 const App = () => {
-  return (
-    <div>
-      {/* <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/home" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/help" exact component={Help} />
-        <Route path="/v1/" exact component={Homev1} />
-      </BrowserRouter> */}
-      <Home />
-    </div>
-  );
+  return <Button variant="contained">Hello World</Button>;
 };
 
 export default App;
